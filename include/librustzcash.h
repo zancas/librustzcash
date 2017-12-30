4e69cbc46aa8b8b7a64ec1435a82ae8ad8c188f9
str4d <str4d@mail.i2p>
2017-12-30 23:36:23 +0100

    Implement Equihash validator
    Follows Zcash implementation as closely as possible.

--- include/librustzcash.h
+++ include/librustzcash.h
@@ -5,6 +5,14 @@
 
 extern "C" {
     uint64_t librustzcash_xor(uint64_t a, uint64_t b);
+    bool librustzcash_eh_isvalid(uint32_t n,
+                                 uint32_t k,
+                                 const unsigned char* input,
+                                 size_t input_len,
+                                 const unsigned char* nonce,
+                                 size_t nonce_len,
+                                 const unsigned char* soln,
+                                 size_t soln_len);
 }
 
 #endif // LIBRUSTZCASH_INCLUDE_H_
--- src/rustzcash.rs
+++ src/rustzcash.rs
@@ -1,5 +1,13 @@
+extern crate blake2b;
+extern crate byteorder;
 extern crate libc;
-use libc::uint64_t;
+#[macro_use]
+extern crate log;
+
+use libc::{c_uchar, size_t, uint32_t, uint64_t};
+use std::slice;
+
+pub mod equihash;
 
 /// XOR two uint64_t values and return the result, used
 /// as a temporary mechanism for introducing Rust into
@@ -7,6 +15,26 @@ use libc::uint64_t;
 #[no_mangle]
 pub extern "system" fn librustzcash_xor(a: uint64_t, b: uint64_t) -> uint64_t {
     a ^ b
+}
+
+#[no_mangle]
+pub extern "system" fn librustzcash_eh_isvalid(
+    n: uint32_t,
+    k: uint32_t,
+    input: *const c_uchar,
+    input_len: size_t,
+    nonce: *const c_uchar,
+    nonce_len: size_t,
+    soln: *const c_uchar,
+    soln_len: size_t,
+) -> bool {
+    if (k >= n) || (n % 8 != 0) || (soln_len != (1 << k) * ((n / (k + 1)) as usize + 1) / 8) {
+        return false;
+    }
+    let rs_input = unsafe { slice::from_raw_parts(input, input_len) };
+    let rs_nonce = unsafe { slice::from_raw_parts(nonce, nonce_len) };
+    let rs_soln = unsafe { slice::from_raw_parts(soln, soln_len) };
+    equihash::is_valid_solution(n, k, rs_input, rs_nonce, rs_soln)
 }
 
 #[test]
