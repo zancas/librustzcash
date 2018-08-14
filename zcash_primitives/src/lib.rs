b01df3ebfd2c0d9919ad2304781b2eba6ce51a7e
Jack Grigg <jack@z.cash>
2018-08-14 23:43:33 +0100

    Refactor Sapling spendAuthSig creation into zcash_primitives::sapling

--- zcash_primitives/src/lib.rs
+++ zcash_primitives/src/lib.rs
@@ -4,10 +4,12 @@ extern crate lazy_static;
 extern crate blake2_rfc;
 extern crate byteorder;
 extern crate pairing;
+extern crate rand;
 extern crate sapling_crypto;
 
 use sapling_crypto::jubjub::JubjubBls12;
 
+pub mod sapling;
 mod serialize;
 pub mod transaction;
 
