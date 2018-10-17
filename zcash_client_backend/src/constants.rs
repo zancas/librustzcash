636ddc082e6cd3c7aafb22864390118d6b9e733f
Jack Grigg <jack@z.cash>
2018-10-17 10:06:13 +0100

    Implement PaymentAddress encoding

---
+++ zcash_client_backend/src/constants.rs
@@ -0,0 +1,2 @@
+pub const HRP_SAPLING_EXTENDED_SPENDING_KEY_MAIN: &str = "zs";
+pub const HRP_SAPLING_EXTENDED_SPENDING_KEY_TEST: &str = "ztestsapling";
--- zcash_client_backend/src/lib.rs
+++ zcash_client_backend/src/lib.rs
@@ -1,3 +1,4 @@
+extern crate bech32;
 extern crate hex;
 extern crate pairing;
 extern crate protobuf;
@@ -5,6 +6,11 @@ extern crate sapling_crypto;
 extern crate zcash_primitives;
 extern crate zip32;
 
+#[cfg(test)]
+extern crate rand;
+
+pub mod address;
+pub mod constants;
 pub mod data;
 pub mod keystore;
 pub mod wallet;
